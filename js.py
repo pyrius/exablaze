d170e225eeda7012be824adfe3a35d01f0393c91
pyrius <pyrius@gmail.com>
2021-06-11 14:48:18 +0200

    revert4

--- js.py
+++ js.py
@@ -1,22 +1,7 @@
 from jsonrpc_requests import Server
 
-<<<<<<< HEAD:js.py
-<<<<<<< HEAD:js.py
-<<<<<<< HEAD:js.py
 fusion = Server("http://EMEA-DCRS-FM-48X-1/jsonrpc")
 fusion.login(username="admin", password="cisco")
-=======
-fusion = Server("http://fusion1/jsonrpc")
-fusion.login(username="admin", password="admin")
->>>>>>> parent of 63ba80f (emea-calo change):jsfusion.py
-=======
-fusion = Server("http://fusion1/jsonrpc")
-fusion.login(username="admin", password="admin")
->>>>>>> parent of 63ba80f (emea-calo change):jsfusion.py
-=======
-fusion = Server("http://fusion1/jsonrpc")
-fusion.login(username="admin", password="admin")
->>>>>>> parent of 63ba80f (emea-calo change):jsfusion.py
 #print (fusion.get_hostname())
 #print (fusion.get_startup_config("patch"))
 #print (fusion.get_messages())
